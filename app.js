62d878f10ab00dc13b76cff08fe8ce4fe86405d2
A.Umesh <108319125+Aveti-Umesh@users.noreply.github.com>
2026-02-07 14:48:08 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 // Root endpoint
 app.get("/", (req, res) => {
-  res.send("🚀 DevOps Demo Application is Running Successfully!");
+  res.send("DevOps Demo Application is Running Successfully! - Using Nodejs");
 });
 
 // Health check endpoint
